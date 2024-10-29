702c41db08083f1dc8232a7832ce6b7003a18d44
Nwabueze John <143359560+Johnnwabueze01@users.noreply.github.com>
2024-10-29 11:18:13 -0700

    Add files via upload

--- src/Compenents/Works/works.jsx
+++ src/Compenents/Works/works.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './works.css'
-import portfolio1 from '../../assets/portfolio-1.png'
-import portfolio2 from '../../assets/portfolio-2.png'
-import portfolio3 from '../../assets/portfolio-3.png'
-import portfolio4 from '../../assets/portfolio-4.png'
+import Project1 from '../../assets/Screenshot e.png'
+import project2 from '../../assets/Screenshot M.png'
+import project3 from '../../assets/Screenshot T.png'
+import project4 from '../../assets/Screenshot EB.png'
 import portfolio5 from '../../assets/portfolio-5.png'
 import portfolio6 from '../../assets/portfolio-6.png'
+import Go from '../../assets/go-to.png'
 
 
 const Works = () => {
@@ -17,12 +18,54 @@ const Works = () => {
          efficient software tools, my work highlights my technical skills, creativity, and commitment
           to delivering impactful results that drive value and innovation.</span>
           <div className="worksimgs">
-            <img src={portfolio1} alt="" className="workimg" />
-            <img src={portfolio2} alt="" className="workimg" />
-            <img src={portfolio3} alt="" className="workimg" />
-            <img src={portfolio4} alt="" className="workimg" />
-            <img src={portfolio5} alt="" className="workimg" />
-            <img src={portfolio6} alt="" className="workimg" />
+            <div className="workimg">
+            <img src={Project1} alt="" className="img" />
+            <hr />
+            <div className="txt">
+              <h3>Styles Nest</h3> 
+              <a href="https://styles-nest.vercel.app/"><p>live link at <img className='i' src={Go} alt="" /></p></a>
+            </div>
+            </div>
+            <div className="workimg">
+            <img src={project2} alt="" className="img" />
+            <hr />
+            <div className="txt">
+              <h3>Meme-Generator</h3> 
+              <a href="https://meme-generator-zeta-wheat.vercel.app/"><p>live link at <img className='i' src={Go} alt="" /></p></a>
+            </div>
+            </div>
+            <div className="workimg">
+            <img src={project3} alt="" className="img" />
+            <hr />
+            <div className="txt">
+              <h3>To-Do App</h3> 
+              <a href="https://to-do-list-rho-fawn.vercel.app/"><p>live link at <img className='i' src={Go} alt="" /></p></a>
+            </div>
+            </div>
+            <div className="workimg">
+            <img src={project4} alt="" className="img" />
+            <hr />
+            <div className="txt">
+              <h3>Easy-Bank Landing Page</h3> 
+              <a href="https://easy-bank-one-beta.vercel.app/"><p>live link at <img className='i' src={Go} alt="" /></p></a>
+            </div>
+            </div>
+            <div className="workimg">
+            <img src={Project1} alt="" className="img" />
+            <hr />
+            <div className="txt">
+              <h3>Styles Nest</h3> 
+              <a href="https://styles-nest.vercel.app/"><p>live link at <img className='i' src={Go} alt="" /></p></a>
+            </div>
+            </div>
+            <div className="workimg">
+            <img src={Project1} alt="" className="img" />
+            <hr />
+            <div className="txt">
+              <h3>Styles Nest</h3> 
+              <a href="https://styles-nest.vercel.app/"><p>live link at <img className='i' src={Go} alt="" /></p></a>
+            </div>
+            </div>
           </div>
           <button className="workbtn">See More</button>
     </section>
